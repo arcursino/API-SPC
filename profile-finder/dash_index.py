7226eae2dde9bee0bc0191646824bb29c8f2a6b3
guilhermemigliano <44805868+guilhermemigliano@users.noreply.github.com>
2021-06-04 17:12:39 -0300

    Projeto atualizado

--- profile-finder/dash_index.py
+++ profile-finder/dash_index.py
@@ -22,6 +22,73 @@ from app import app
 import plotly.express as px
 from dash.exceptions import PreventUpdate
 
+from sklearn.manifold import TSNE
+
+from biblioteca_recomendacao import get_content_based_recommendations
+
+
+
+
+##############
+
+from sklearn.preprocessing import  MinMaxScaler 
+from sklearn.cluster import KMeans
+from sklearn.decomposition import  PCA
+from mpl_toolkits.mplot3d import Axes3D
+
+import matplotlib.pyplot as plt
+
+import asyncio
+
+
+df2 = pd.read_csv('assets/olist_segmentado.csv')
+
+df3 = pd.read_csv('assets/olist_processado.csv')
+
+
+#Normalização com média 0 e desvio 1
+normaliza = MinMaxScaler()
+
+#Aplica a normalização
+#X_std = normaliza.fit_transform(df2)
+
+
+#transformando os dados normalizados em um dataframe
+#df2 = pd.DataFrame(X_std, columns=['ano', 'mes', 'customer_city_codes', 'customer_state_codes'])
+
+#Realizando a aplicação do Kmeans
+km = KMeans(n_clusters=4)
+clusters = km.fit_predict(df2)
+df2['cluster'] = clusters
+
+
+#plota o gráfico de dispersão
+fig4 = plt.figure(figsize=(20,10))
+ax = fig4.add_subplot(111, projection='3d')
+
+x = df2['ano']
+y = df2['mes']
+z = df2['customer_city_codes']
+
+ax.scatter(x, y, z, marker='s', c=df2['cluster'], s=40, cmap='tab10')
+
+plt.xlabel('Ano')
+plt.ylabel('Mês')
+ax.set_zlabel('Cidade')
+
+fig4 = px.scatter_3d(df2, x= x, y=y, z=z,
+                    color='cluster', symbol='cluster')
+
+
+
+#############
+
+
+
+
+
+
+
 # Create map object
 
 df = pd.read_csv('assets/dataset2.csv')
@@ -37,8 +104,8 @@ colunas = df.columns
 
 veteranos = df[df['geracoes'] == '1900 a 1940 - veteranos']
 boomers = df[df['geracoes'] == '1940 a 1959 - baby boomers ']
-ger_x = df[df['geracoes'] == '1960 a 1979 - geração y']
-ger_y = df[df['geracoes'] == '1980 a 1994 - geração x']
+ger_x = df[df['geracoes'] == '1980 a 1994 - geração y']
+ger_y = df[df['geracoes'] == '1960 a 1979 - geração x']
 ger_z = df[df['geracoes'] == '1994 a 2010 - geração z']
 
 trace1 = go.Bar(
@@ -101,8 +168,8 @@ fig1.update_layout(
 fig1.update_traces(texttemplate="%{text:.2s}")  # text formart
 
 
-app.layout = html.Div(
-    children=[
+app.layout = html.Div(className="div-wrapper",
+    children=[        
         html.Div(className="top-bar",
                  children=[
                      html.P(className="logo", children=["Profile Finder"])
@@ -110,10 +177,10 @@ app.layout = html.Div(
                  ),
         html.Div(className="menu-bar-left",
                  children=[
-                     html.Div(style={'width': '100%', 'height': '45px', 'float': 'left', 'borderLeft': '3px solid yellow', 'marginTop': '10px'},
+                     html.Div(style={'width': '100px', 'height': '100%', 'float': 'left',  'marginTop': '10px'},
                               children=[
                          html.Img(src="/assets/imgs/stats.png", width="35px",
-                                  height="35px", style={'marginTop': '5px', 'marginLeft': '10px'})
+                                  height="35px", style={'marginTop': '5px', 'paddingLeft': '10px', 'borderLeft': '3px solid yellow'})
                      ]),
 
                  ]),  # menu1
@@ -125,7 +192,7 @@ app.layout = html.Div(
                                   children=[html.Span(className="filter-title-span", children=['Localização'])
                                             ]),
                          html.P(className='estadoTitle', children=['Estado:']),
-                         dcc.Dropdown(id='state_dropdown', className="filter-dropdown", style={'backgroundColor': 'transparent', 'borderColor': '#315475'},
+                         dcc.Dropdown(id='state_dropdown', className="filter-dropdown", style={ 'borderColor': '#315475'},
                                       options=[{'label': i.upper(), 'value': i}
                                                for i in estado],
                                       value=[],
@@ -134,7 +201,7 @@ app.layout = html.Div(
                                       multi=False
                                       ),
                          html.P(className='estadoTitle', children=['Cidade:']),
-                         dcc.Dropdown(id='city_dropdown', className="filter-dropdown", style={'backgroundColor': 'transparent', 'borderColor': '#315475'},
+                         dcc.Dropdown(id='city_dropdown', className="filter-dropdown", style={ 'borderColor': '#315475'},
                                       options=[],
                                       value=[],
                                       disabled=False,
@@ -200,12 +267,62 @@ app.layout = html.Div(
                     style_cell={'textAlign': 'center'},     
                     style_header={'backgroundColor': 'rgb(206, 206, 206)', 'fontWeight': 'bold'},          
                     )]
-            ),            
+            ),
+            dcc.Tab(
+                label='Recomendação de produtos',
+                value='tab-3',
+                className='custom-tab',
+                selected_className='custom-tab--selected',
+                children=[
+                    html.Div(className='grafico-cluster', children=[
+                        dcc.Graph(
+                        style={'height': '500px', 'borderColor': '#315475'},
+                        id="teste123",
+                        figure = fig4,
+                        )
+
+                    ]),
+                    html.Div(className='passo-a-passo', children=[
+                        html.H1(style={'float' : 'left', 'width': '100%', 'marginTop': '50px'},children=["Passo 1"]),
+                        html.P(style={'float' : 'left', 'width': '100%'}, children=["Selecione um dos clusters:"]),
+                        dcc.Dropdown(id='clusters_dropdown', className="filter-dropdown", style={'borderColor': '#315475'},
+                                      options=[
+                                        {'label': 'Cluster 0', 'value': '0'},
+                                       {'label': 'Cluster 1', 'value': '1'},
+                                       {'label': 'Cluster 2', 'value': '2'},
+                                       {'label': 'Cluster 3', 'value': '3'}],
+                                      value=[],
+                                      disabled=False,
+                                      clearable=True,
+                                      multi=False
+                        ),
+                        html.H1(style={'float' : 'left', 'width': '100%', 'marginTop': '50px'}, children=["Passo 2"]),
+                        html.P(style={'float' : 'left', 'width': '100%'}, children=["Selecione um dos produtos:"]),
+                        dcc.Dropdown(id='produtos_dropdown', className="filter-dropdown", style={'borderColor': '#315475'},
+                                      options=[],
+                                      value=[],
+                                      disabled=False,
+                                      clearable=True,
+                                      multi=False
+                        ),
+                        html.Button('Pesquisar', id='submit-val', n_clicks=0, className="btn-pesquisar")
+
+                    ]),
+                    html.Div(id='recomendacao', className='recomendacao-produtos', children=[dash_table.DataTable(
+                    id='table1',
+                    columns=[{"name": "Produtos recomendados", "id": '0'}],    
+                    style_cell={'textAlign': 'center'},     
+                    style_header={'backgroundColor': 'rgb(206, 206, 206)', 'fontWeight': 'bold'},          
+                    )])
+                ]
+            ),              
         ]),
              
         ]),
 
         html.Div(id='tabs-content-classes'),
+
+        
         
 
         
@@ -316,6 +433,7 @@ def groupby_cidade(cidade, estado):
         ger_z = filtered_data[filtered_data['geracoes']
                             == '1994 a 2010 - geração z']
 
+
         trace1 = go.Bar(
             x=veteranos['renda'].unique(),
             y=veteranos.groupby('renda')['geracoes'].count(),
@@ -385,20 +503,70 @@ def groupby_cidade(cidade, estado):
 def set_cities_options(estado):
     return [{'label': i.upper(), 'value': i} for i in df.loc[df["SIGLA_UF"] == estado, "NM_MUN"].unique()]
 
-
 @app.callback(
     Output('table', 'data'),
     Input('city_dropdown', 'value'))
 def update_table(cidade):
-    cidade = df.loc[df["NM_MUN"] == cidade,
-                'top10_produtos_cidade'].unique()
-    prod = cidade[0].strip('][').split(', ')
+    cidade = df3.loc[df3["customer_city"] == cidade,
+                'product_category_name'].unique()
+    # print(cidade[0])
+    # prod = cidade[0].strip('][').split(', ')
     new = []
-    for x in prod:
-        new.append(x.replace("'", ""))
+    i = 0
+    for x in cidade:
+        if i < 10:
+            new.append(x)
+            i += 1
 
     df1 = pd.DataFrame(new)
     return df1.to_dict('rows')
+
+
+@app.callback(
+    Output('produtos_dropdown', 'options'),
+    Input('city_dropdown', 'value'))
+    
+def set_prods_options(cidade):
+    cidade = df3.loc[df3["customer_city"] == cidade,
+                'product_category_name'].unique()
+    new = []
+    i = 0
+    for x in cidade:
+        if i <= 10:
+            new.append(x)
+            i += 1
+    return [{'label': i, 'value': i} for i in new]
+    
+
+# @app.callback(
+#     Output('table1', 'data'),
+#     Input('clusters_dropdown', 'value'),
+#     Input('produtos_dropdown', 'value'))
+# def recomenda(cluster, prod):
+#     if cluster and prod:
+#         prod1 = get_content_based_recommendations(int(cluster), prod)
+#         df1 = pd.DataFrame(prod1)
+#         new = []
+#         for x in df1.to_dict('rows'):
+#             new.append("produto: " + x['product_id'] + " | categoria: " + x['product_category_name'])
+#             #print(x['product_category_name'])
+#         df = pd.DataFrame(new)
+#         return df.to_dict('rows')
+
+@app.callback(
+    dash.dependencies.Output('table1', 'data'),
+    [dash.dependencies.Input('submit-val', 'n_clicks')],
+    [dash.dependencies.State('produtos_dropdown', 'value'), dash.dependencies.State('clusters_dropdown', 'value')])
+def update_output(n_clicks, prod, cluster):
+    prod1 = get_content_based_recommendations(int(cluster), prod)
+    df1 = pd.DataFrame(prod1)
+    new = []
+    for x in df1.to_dict('rows'):
+        new.append("produto: " + x['product_id'] + " | categoria: " + x['product_category_name'])
+        #print(x['product_category_name'])
+    df = pd.DataFrame(new)
+    return df.to_dict('rows')
+    
 
 host = socket.gethostbyname(socket.gethostname())
 
